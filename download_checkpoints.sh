017dde07b32415c7f4e7decbbeb7da72cf9114f6
Mohamed Emam <126331291+mohamed-em2m@users.noreply.github.com>
2025-11-26 02:35:55 +0200

    Add commands to download additional models

--- download_checkpoints.sh
+++ download_checkpoints.sh
@@ -1,3 +1,9 @@
 gdown --id 1rvWuM12cyvNvBQNCLmG4Fr2L1rpjQBF0
 
 mv float.pth checkpoints/
+hf download r-f/wav2vec-english-speech-emotion-recognition \
+  --local-dir ./checkpoints/wav2vec-english-speech-emotion-recognition \
+  --include "*"
+hf download facebook/wav2vec2-base-960h \
+  --local-dir ./checkpoints/facebook--wav2vec2-base-960h \
+  --include "*"
