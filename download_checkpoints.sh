d908959ebbd9908a4f2ae483e08f302273cddf38
Mohamed Emam <126331291+mohamed-em2m@users.noreply.github.com>
2025-11-26 04:09:00 +0200

    Fix local directory path for wav2vec2 model download

--- download_checkpoints.sh
+++ download_checkpoints.sh
@@ -5,5 +5,5 @@ hf download r-f/wav2vec-english-speech-emotion-recognition \
   --local-dir ./checkpoints/wav2vec-english-speech-emotion-recognition \
   --include "*"
 hf download facebook/wav2vec2-base-960h \
-  --local-dir ./checkpoints/facebook--wav2vec2-base-960h \
+  --local-dir ./checkpoints/wav2vec2-base-960h \
   --include "*"
